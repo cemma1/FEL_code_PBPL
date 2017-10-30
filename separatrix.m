8d4153d7918a97e7231161a15dfd169300758854
Claudio Emma <32077567+cemma1@users.noreply.github.com>
2017-10-29 22:10:11 -0700

    Deleted separatrix it's now in the post-processor

--- separatrix.m
+++
@@ -1,6 +0,0 @@
-function [ sep ] = separatrix( psi,psir )
-% Calculate height of separatrix at phase psi for resonant phase psir
-% Detailed explanation goes here
-sep = sqrt(abs(cos(psi)+cos(psir)+(psi+psir-pi*sign(psir))*sin(psir)));
-end
-
