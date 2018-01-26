c71ac505b019e092064b09284396fe49c8ed69b2
cemma1 <cemma@slac.stanford.edu>
2018-01-25 16:32:01 -0800

    added updates to postprocessor

--- perave_postprocessor_7h.m
+++ perave_postprocessor_7h.m
@@ -277,7 +277,7 @@ enhance_plot
 end
 %% Functions to calculate bucket parameters and fit the gain length
 
-function [ sep ] = separatrix( psi,psir )
+function [sep] = separatrix(psi,psir)
 % Calculate height of separatrix at phase psi for resonant phase psir
 % Detailed explanation goes here
 sep = sqrt(abs(cos(psi)+cos(psir)+(psi+psir-pi*sign(psir))*sin(psir)));
