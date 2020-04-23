3ef1173c1211ec89fe2a45d3b0814d6dfcd3550a
cemma1 <cemma@slac.stanford.edu>
2020-04-22 21:37:45 -0700

    added MA parameters

--- Perave_User_Input_7p.m
+++ Perave_User_Input_7p.m
@@ -6,15 +6,14 @@ param.K = 3.5; %e0*Bfield*/me/c/ku;                         % Peak undulator par
 param.ku = 2.*pi./param.lambdau;                            % undulator wavenumber
 lwig=param.lambdau*30;                                     % Undulator length m    
 % Tapering options
-param.tapering = 1;                                         % tapering (0 no tapering ; 1 decelation)    
-param.z0 = param.lambdau*500;
+param.tapering = 0;                                         % tapering (0 no tapering ; 1 decelation)    
+param.z0 = param.lambdau;
 param.psir = pi/4;
-
 % Polynomial Taper options
 param.order = 1;
 param.ratio = 0.03/(lwig-param.z0)^param.order;
 %% Simulation control options
-param.phasespacemovie=1;
+param.phasespacemovie=0;
 param.itdp = 1;
 param.saveoutput=0;
 % Set simulation length and # of snapshots
@@ -32,16 +31,16 @@ end
 %% radiation parameters
 param.lambda0 = 2.5e-9;                                    % Seed wavelength
 param.k = 2*pi/param.lambda0;                                 % wavenumber in free space
-P0 = 1e4; param.P0=P0;                                        % Seed power (W) 
+P0 = 1e5; param.P0=P0;                                        % Seed power (W) 
 zr = 5;                                                       % Rayleigh length of seed
 param.waist = sqrt(zr*param.lambda0/pi);
 A_mode = pi*param.waist^2/2;
 param.E0 = sqrt(P0/c/eps0/A_mode);                        % Assume circular polarization  
 %% Electron beam parameters
 param.gamma0 = sqrt(param.k/2/param.ku*(1+param.K^2/2));      % relativistic gamma factor
-param.Np = 1024;                                               % # of macroparticles (500-1000 well) 
+param.Np = 8192;                                              % # of macroparticles (500-1000 well) 
 param.Ee = param.gamma0*me*c^2/e0;                            % Total e-beam energy (eV)
-energyspread = 33;                                           % Absolute energy spread MeV
+energyspread = 10;                                            % Absolute energy spread MeV
 param.deltagammarel = energyspread/param.gamma0/0.511;        % Relative energy spread dgamma/gamma
 param.deltagamma = param.gamma0*param.deltagammarel;
 param.prebunching = 0;                                        % set to 1 to start from a pre-bunched beam. 
@@ -61,7 +60,7 @@ param.A_e = 2*pi*param.sigmax^2;                                  % beam cross s
 bunchlength=param.nslices*param.zsep*param.lambda0/c;
 % To include a non-uniform current distribution 
 param.currprofile = 1;                                        % 0 = uniform current profile 1 = gaussian 2 = trapezoid
-param.sigmat = 33*1e-18;                                      % beam sigma [s], only for gaussian (param.currprofile =1)
+param.sigmat = 20e-18;                                        % beam sigma [s], only for gaussian (param.currprofile =1)
 param.currgradient = param.I;                                 % Slope of trapezoidal current distribution
 %% Simplifying constants
 %param.chi2 = e0/me/c^2; % These are the helical ones
@@ -69,7 +68,6 @@ param.currgradient = param.I;                                 % Slope of trapezo
 %const_resp=1/param.chi2*(param.lambdau/2/param.lambda0);
 
 param.chi2 = e0/me/c^2/2; % These are the planar ones
-param.chi1=mu0*c/2*param.I/param.A_e; %These are the planar ones
 % Constant for the resonant phase calculation   
 const_resp=1/param.chi2*(param.lambdau/2/param.lambda0);
 % This is not right for the planar case it has to be multipled by JJ(z) and
